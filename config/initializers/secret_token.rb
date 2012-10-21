561b75e92791fb55679388757aa7c87df58f7c43
Andrés Ricardo Torres Martínez <andresrtm@localhost.localdomain>
2012-10-21 18:50:44 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TISProject::Application.config.secret_token = '8856500541c9dcb703fc06ae99f3ae8046b4bbb1e4d882517575d348dcf5e1e664b54a9c9210b2386a906f6c72aeb970f68e1fdbb21672a519fd6ba8a759fd59'
