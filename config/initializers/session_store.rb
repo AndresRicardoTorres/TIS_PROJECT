e390cd698d7111e8b102faf455e4c19976199e15
Andrés Ricardo Torres Martínez <andresrtm@zeti6>
2012-10-23 15:38:03 -0500

    borro el rails generado

--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-TISProject::Application.config.session_store :cookie_store, key: '_TIS_PROJECT_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# TISProject::Application.config.session_store :active_record_store
