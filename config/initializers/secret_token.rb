c82456d3d43d11dda494bd2a97afa7dda0043a64
Andrés Ricardo Torres Martínez <andresrtm@zeti6>
2012-10-23 15:43:18 -0500

    instalacion de rails limpio con mongo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TISProject::Application.config.secret_token = '91214e3615e336ad38dd0627a8ef4c97e6dafc2c070850c2a9162d25c49c5c69ec6e926f57dab704bba16c2a3664ee124b2e626556daac9ad229bf2868f45e85'
