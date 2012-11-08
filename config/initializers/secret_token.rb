3b02ed2c93833b3ba83543dddf6d4d1c3d61cf6b
Andres Ricardo Torres Martinez <ricardo.torres@correounivalle.edu.co>
2012-11-08 13:39:29 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TISProject::Application.config.secret_token = 'b0209f6fcdd4f8c05203ed129c5bcefefa88f0bea648c544853ad499ad8b485b8f91f22a83749c6ac99b0278c3c6b6152436e302d514d75ec604d1e37f1bde71'
