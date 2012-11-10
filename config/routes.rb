08b39f6e9fdb0aa04d25d5e68104afcf68efb071
Andrés Ricardo Torres Martínez <andresrtm@localhost.localdomain>
2012-11-10 10:53:45 -0500

    Cambio el mongoid.yml y el scaffold de proyectos

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 TISProject::Application.routes.draw do
+  resources :projects
+
   authenticated :user do
     root :to => 'home#index'
   end
   root :to => "home#index"
+  
   devise_for :users
   resources :users
 end
