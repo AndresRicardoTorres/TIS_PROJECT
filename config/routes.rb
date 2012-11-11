6731bbf4b40a9ba0b618dbf6ad4479108d290462
Andrés Ricardo Torres Martínez <ricardo.torres@correounivalle.edu.co>
2012-11-10 23:47:47 -0500

    Se arregla que las rutas crearan ciclo infinito

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 TISProject::Application.routes.draw do
-  
+
   devise_for :users
   resources :users
   resources :user_histories
   resources :modulos
   resources :projects
 
-  # devise_scope :user do
-    # get "/", :to => "devise/sessions#new"
-  # end
-
   authenticated :user do
     root :to => 'home#index'
+
   end
-      root :to => 'home#index'
+
+  devise_scope :user do
+    get "/", :to => "devise/sessions#new"
+  end
+
 end
