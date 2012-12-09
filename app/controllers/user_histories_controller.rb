1a31aacd1602db4cdba54bd0a887df0fdabfb7d3
Mayra C <mayalces@gmail.com>
2012-12-09 17:31:07 -0500

    Cambios en Historias de Usuarios
    CRUD completo

--- app/controllers/user_histories_controller.rb
+++ app/controllers/user_histories_controller.rb
@@ -48,7 +48,8 @@ class UserHistoriesController < ApplicationController
 
   # GET /user_histories/1/edit
    def edit
-    @user_history = UserHistory.find(params[:id])
+	@project = Project.find(params[:project_id])
+    @user_history = @project.user_histories.find(params[:id])
   end 
 
   # POST /user_histories
@@ -68,27 +69,26 @@ class UserHistoriesController < ApplicationController
   # PUT /user_histories/1
   # PUT /user_histories/1.json
   def update
+	@project = Project.find(params[:project_id])
     @user_history = UserHistory.find(params[:id])
-
-    respond_to do |format|
-      if @user_history.update_attributes(params[:user_history])
-        format.html { redirect_to @user_history, notice: 'User history was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @user_history.errors, status: :unprocessable_entity }
-      end
+	
+	if @user_history.update_attributes(params[:user_history])
+        redirect_to project_user_history_url(@project, @user_history) #, notice: 'User history was successfully updated.' }
+    else
+        render action: "edit" 
+        #format.json { render json: @user_history.errors, status: :unprocessable_entity }
     end
   end 
 
   # DELETE /user_histories/1
   # DELETE /user_histories/1.json
    def destroy
+	@project = Project.find(params[:project_id])
     @user_history = UserHistory.find(params[:id])
     @user_history.destroy
 
     respond_to do |format|
-      format.html { redirect_to user_histories_url }
+      format.html { redirect_to project_path(@project) }
       format.json { head :no_content }
     end
   end 
